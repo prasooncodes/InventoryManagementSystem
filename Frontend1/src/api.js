0af66cace14a2e31298a3716b3affe401f9ca59f
Prasoon <prasoonmishra9936@gmail.com>
2025-07-15 18:56:02 +0530

    fix: ensure withCredentials is set for axios instance to support cross-origin requests

--- Frontend1/src/api.js
+++ Frontend1/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE, // Backend URL from .env file 
+  baseURL: import.meta.env.VITE_API_BASE,
+  withCredentials: true
 });
 
 export default API;
